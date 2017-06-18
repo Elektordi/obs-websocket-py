8abfd04b77e255fed62daa24e08a99b90b0f0926
Guillaume Genty <elektordi@elektordi.net>
2017-06-18 20:56:18 +0200

    Real logging instead of print calls

--- obswebsocket/core.py
+++ obswebsocket/core.py
@@ -10,6 +10,9 @@ import base64
 import threading
 import time
 
+import logging
+LOG = logging.getLogger(__name__)
+
 from . import exceptions
 
 class Core:
@@ -23,7 +26,9 @@ class Core:
     def connect(self, host, port):
         try:
             self.ws = websocket.WebSocket()
+            LOG.info("Connecting...")
             self.ws.connect("ws://%s:%d"%(host, port))
+            LOG.info("Connected!")
         except socket.error, e:
             raise exceptions.ConnectionFailure(str(e))
     
@@ -32,6 +37,7 @@ class Core:
         raise exceptions.ConnectionFailure("Reconnect not implemented")
             
     def disconnect(self):
+        LOG.info("Disconnecting...")
         if not self.thread_recv is None:
             self.thread_recv.running = False
         try:
@@ -67,6 +73,7 @@ class Core:
         id = str(self.id)
         self.id += 1
         data["message-id"] = id
+        LOG.debug("Sending message id %s: %r"%(id, data))
         self.ws.send(json.dumps(data))
         return self.waitmessage(id)
         
@@ -95,16 +102,20 @@ class RecvThread(threading.Thread):
                 message = self.ws.recv()
                 result = json.loads(message)
                 if 'update-type' in result:
+                    LOG.debug("Got message: %r"%(result))
                     self.core.eventmanager.trigger(result)
                 elif 'message-id' in result:
+                    LOG.debug("Got answer for id %s: %r"%(result['message-id'], result))
                     self.core.answers[result['message-id']] = result
                 else:
-                    print "Unknow message: %r"%(result)
+                    LOG.warning("Unknow message: %r"%(result))
             except websocket.WebSocketConnectionClosedException:
                 if self.running:
                     self.core.reconnect()
             except ValueError:
-                print "Invalid message: %s"%(message)
+                LOG.warning("Invalid message: %s"%(message))
+        # end while
+        LOG.debug("RecvThread ended.")
             
             
 class EventManager:
--- samples/events.py
+++ samples/events.py
@@ -4,6 +4,9 @@
 import sys
 import time
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 sys.path.append('../')
 from obswebsocket import obsws
 
