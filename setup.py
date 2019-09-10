32ac0829bcfba31621686e15b0953d94a0648c71
Adrian Orive <adrian.orive.oneca@gmail.com>
2019-09-10 10:56:37 +0200

    Indentation and no spaces between '=' for keyword arguments (PEP8)
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- setup.py
+++ setup.py
@@ -30,31 +30,31 @@ class UpdateClasses(sdist):
 
 
 setup(
-  name = 'obs-websocket-py',
-  packages = ['obswebsocket'],
-  cmdclass = {'sdist': UpdateClasses},
-  license = 'MIT',
-  version = version,
-  description = 'Python library to communicate with an obs-websocket server.',
-  long_description = description,
-  author = 'Guillaume "Elektordi" Genty',
-  author_email = 'elektordi@elektordi.net',
-  url = 'https://github.com/Elektordi/obs-websocket-py',
-  download_url = 'https://github.com/Elektordi/obs-websocket-py/archive/%s.tar.gz'%(version),
-  keywords = ['obs', 'obs-studio', 'websocket'],
-  classifiers = [
-    'License :: OSI Approved :: MIT License',
-    'Environment :: Plugins',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Libraries',
+    name='obs-websocket-py',
+    packages=['obswebsocket'],
+    cmdclass={'sdist': UpdateClasses},
+    license='MIT',
+    version=version,
+    description='Python library to communicate with an obs-websocket server.',
+    long_description=description,
+    author='Guillaume "Elektordi" Genty',
+    author_email='elektordi@elektordi.net',
+    url='https://github.com/Elektordi/obs-websocket-py',
+    download_url='https://github.com/Elektordi/obs-websocket-py/archive/{}.tar'
+                 '.gz'.format(version),
+    keywords=['obs', 'obs-studio', 'websocket'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
 
-    'Development Status :: 4 - Beta',
+        'Development Status :: 4 - Beta',
 
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-  ],
-
-  install_requires=['websocket-client'],
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=['websocket-client', 'six'],
 )
