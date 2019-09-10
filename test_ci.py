cfa6dcbba2993b4658c3485a13640b03ad20f8b6
Adrian Orive <adrian.orive.oneca@gmail.com>
2019-09-10 11:39:45 +0200

    Two blank lanes before module level funcions (PEP8)
    Signed-off-by: Adrian Orive <adrian.orive.oneca@gmail.com>

--- test_ci.py
+++ test_ci.py
@@ -4,13 +4,16 @@ host = "127.0.0.1"
 port = 4444
 password = "secret"
 
+
 def test_load():
     ws = obsws(host, port, password)
     # Just test everything is ok with the object...
 
+
 def test_build_ok_requests():
     r = requests.GetVersion()
     assert r.name=="GetVersion"
+
 
 def test_build_ok_events():
     e = events.Heartbeat()
