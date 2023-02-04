fcd7c6efd9a34ba4c62c59a130df211f6dd7776f
Guillaume Genty <elektordi@elektordi.net>
2023-02-04 23:12:39 +0100

    Python 3.6 has reached EOL

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'Development Status :: 4 - Beta',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
