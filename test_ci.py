00f39fcfa4afe08db052b34f78cbd6c33480732a
Guillaume Genty <elektordi@elektordi.net>
2023-02-07 00:43:09 +0100

    Mock OBS websocket for CI tests (fix #9)

--- test_ci.py
+++ test_ci.py
@@ -1,20 +1,67 @@
+from unittest.mock import patch, Mock
+from queue import Queue, Empty
+import time
+
 from obswebsocket import obsws, requests, events
 
-host = "127.0.0.1"
-port = 4444
-password = "secret"
+
+def fake_send(data):
+    if data == '{"request-type": "GetAuthRequired", "message-id": "1"}':
+        fake_recv.queue.put('{"status":"ok"}')
+    elif data == '{"request-type": "FakeRequest", "message-id": "2"}':
+        fake_recv.queue.put('{"message-id": "2", "status":"ok"}')
+    else:
+        raise Exception(data)
 
 
-def test_load():
-    _ = obsws(host, port, password)
-    # Just test everything is ok with the object...
+def fake_recv():
+    try:
+        return fake_recv.queue.get(timeout=1)
+    except Empty:
+        return ""
+fake_recv.queue = Queue()   # noqa: E305
 
 
-def test_build_ok_requests():
-    r = requests.GetVersion()
-    assert r.name == "GetVersion"
+def test_request():
+    ws = obsws("127.0.0.1", 4444, "")
+    with patch('websocket.WebSocket') as mock:
+        mockws = mock.return_value
+        mockws.send = Mock(wraps=fake_send)
+        mockws.recv = Mock(wraps=fake_recv)
+
+        ws.connect()
+        mockws.connect.assert_called_once_with("ws://127.0.0.1:4444")
+        assert ws.thread_recv.running
+
+        r = ws.call(requests.FakeRequest())
+        assert r.name == "FakeRequest"
+        assert r.status
+
+        ws.disconnect()
+        assert not ws.thread_recv
 
 
-def test_build_ok_events():
-    e = events.Heartbeat()
-    assert e.name == "Heartbeat"
+def test_event():
+    ws = obsws("127.0.0.1", 4444, "")
+    with patch('websocket.WebSocket') as mock:
+        mockws = mock.return_value
+        mockws.send = Mock(wraps=fake_send)
+        mockws.recv = Mock(wraps=fake_recv)
+
+        ws.connect()
+        mockws.connect.assert_called_once_with("ws://127.0.0.1:4444")
+        assert ws.thread_recv.running
+
+        def on_fake_event(message):
+            assert message.name == "FakeEvent"
+            assert message.getFakeKey() == "fakeValue"
+            on_fake_event.ok = True
+
+        on_fake_event.ok = False
+        ws.register(on_fake_event, events.FakeEvent)
+        fake_recv.queue.put('{"update-type": "FakeEvent", "fakeKey":"fakeValue"}')
+        time.sleep(1)
+        assert on_fake_event.ok
+
+        ws.disconnect()
+        assert not ws.thread_recv
